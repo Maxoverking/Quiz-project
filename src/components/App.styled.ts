d3f31e0671bd6d26a5614299e155ef443f173a1d
Maxoverking <avemax7@gmail.com>
2023-06-23 00:42:13 +0200

    add font & color

---
+++ src/components/App.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const DIV = styled.div`
+  font-family :"Abel" ;
+  margin: 20px;
+  background: var(--pink);
+
+`;
+export const H1 = styled.h1`
+  font-family :"Atma" ;
+  margin: 20px;
+  background: var(--saffron);
+`;
+export const P = styled.p`
+  font-family :"Abel" ;
+  margin: 20px;
+  background: var(--vista-blue);
+  letter-spacing: 0.7px;
+`;
---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import './App.styled'
+import { DIV,H1,P } from './App.styled'
+
+function App() {
+
+
+  return (
+    <>
+      <DIV>dfgfFFFFFFFFFFFFg</DIV>
+      <H1>GSttrhradvdfd</H1>
+      <P>sdfsdfnrkujrhwteymryjry</P>
+    </>
+  )
+}
+
+export default App
